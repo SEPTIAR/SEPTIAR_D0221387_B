9fc374b35e396719cc342d10c595b763d3633c9e
SEPTIAR <100009141+SEPTIAR@users.noreply.github.com>
2022-03-06 00:27:10 +0800

    Day15_java_substring.java

---
+++ Day15_java_substring.java
@@ -0,0 +1,18 @@
+package rascoding;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+
+public class day15_Java_substring {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        String sub = S.substring(start, end);
+        System.out.println(sub);
+    }
+}
